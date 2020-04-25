fcd8a45a3816cf8b4d70455d3f9b6bc28150e027
TroyG <troy.gidney@lethbridgecollege.ca>
2020-04-24 23:53:29 -0600

    Added a api route to list all the available routes Added IP depend to grab the network id automatically, should change this to a config option

--- src/server.js
+++ src/server.js
@@ -4,22 +4,32 @@ const router = express.Router();
 const https = require('https');
 const fs = require('fs');
 const app = express();
+const ip = require('ip');
 
 app.use(cors());
+
 
 router.get('/', (req, res) => {
 	res.status(403).send();
 });
 
+router.get('/api/', (req, res) => {
+	//TODO Authorize
+	res.send(router.stack);
+});
+
 app.use('/', router);
 
+keyPath = './assets/privkey.pem';
+certPath = './assets/fullchain.pem';
+
 const options = {
-	key: fs.readFileSync('/etc/letsencrypt/live/pxl.plus/privkey.pem'),
-	cert: fs.readFileSync('/etc/letsencrypt/live/pxl.plus/fullchain.pem')
+	key: fs.readFileSync(keyPath),
+	cert: fs.readFileSync(certPath)
 };
 
 const httpsServer = https.createServer(options, app);
 
-httpsServer.listen(420, "pxl.plus");
-console.log('Server running at https://pxl.plus:420/');
+httpsServer.listen(420, ip.address());
+console.log(`Server running at ${ip.address()}`);
 
