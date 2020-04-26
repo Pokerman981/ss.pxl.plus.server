f689cde823515fd3d1afb05c0ba8f02dc2980300
TroyG <troy.gidney@lethbridgecollege.ca>
2020-04-26 00:09:12 -0600

    Added some more

--- src/server.js
+++ src/server.js
@@ -9,49 +9,17 @@ const jwt = require('jsonwebtoken');
 const bodyparser = require('body-parser');
 
 app.use(cors());
-app.use(bodyparser);
+app.use(bodyparser.json());
 
 
-
-router.get('/', (req, res) => {
-    res.status(403).send();
-});
-
-router.get('/api', async (req, res) => {
-    //TODO Authorize
-    let paths = {routes: []};
-    for (let stack of router.stack) {
-        paths.routes.push(stack.route.path);
-    }
-
-    res.send(paths);
-});
-
-privatePath = './assets/private.key';
-
-router.post('/api/login', (req, res) => {
-    console.dir(req.body);
-    const user = req.body.username;
-    const pass = req.body.password;
-
-    if (user === null || pass === null) {
-        res.status(400).send('Unable to find required data!');
-        return;
-    }
-    const key = fs.readFileSync(privatePath);
-    const token = jwt.sign(
-        {data: 'foobar'},
-        key,
-        {expiresIn: '1h', algorithm: 'RS256'}, (err, token) => {
-            res.send({token: token});
-        });
-});
+registerRoutes();
 
 
 app.use('/', router);
 
 keyPath = './assets/privkey.pem';
 certPath = './assets/fullchain.pem';
+port = 420;
 
 const options = {
     key: fs.readFileSync(keyPath),
@@ -60,6 +28,68 @@ const options = {
 
 const httpsServer = https.createServer(options, app);
 
-httpsServer.listen(420, ip.address());
-console.log(`Server running at ${ip.address()}`);
+httpsServer.listen(port, ip.address());
+console.log(`Server running at ${ip.address()}:${port}`);
+
+
+
+
+function registerRoutes() {
+    const appURL = "https://pxl.plus";
+    router.all('*', (req, res, next) => {
+        if (req.headers.origin != appURL) {
+            res.status(403).end();
+            return;
+        }
+
+        next();
+    });
+
+    router.get('/', (req, res) => {
+        res.status(403).send();
+    });
+
+    router.get('/api', async (req, res) => {
+        //TODO Authorize
+        let paths = {routes: []};
+        for (let stack of router.stack) {
+            paths.routes.push(stack.route.path);
+        }
+
+        res.send(paths);
+    });
+
+    const privatePath = './assets/private.key';
+    router.post('/api/login', (req, res) => {
+        const user = req.body.username;
+        const pass = req.body.password;
+
+        if (user == null || pass == null) {
+            res.status(400).send('Unable to find required data!');
+            return;
+        }
+        const key = fs.readFileSync(privatePath);
+        const token = jwt.sign(
+            {data: 'foobar'},
+            key,
+            {expiresIn: '1h', algorithm: 'RS256'}, (err, token) => {
+                res.send({token: token});
+            });
+    });
+
+    router.get('/api/verify', (req, res) => {
+       const token = req.get('token');
+        if (token == null) {
+           res.status(400).send('Error: Missing Token!');
+           return;
+       }
+
+
+
+
+
+    });
+    
+
+}
 
