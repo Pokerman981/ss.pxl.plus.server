e91eb395a497e463ddc1f701aaa9f2fb2a9bc929
TroyG <troy.gidney@lethbridgecollege.ca>
2020-04-28 23:11:26 -0600

    Added mysql connections

--- src/server.js
+++ src/server.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const https = require('https');
 const fs = require('fs');
 const app = express();
-const ip = require('ip');
 const jwt = require('jsonwebtoken');
 const bodyparser = require('body-parser');
+const mysql = require('mysql');
 
 app.use(cors());
 app.use(bodyparser.json());
@@ -17,8 +17,9 @@ registerRoutes();
 
 app.use('/', router);
 
-keyPath = './assets/privkey.pem';
-certPath = './assets/fullchain.pem';
+keyPath = '/etc/letsencrypt/live/ss.pxl.plus/privkey.pem';
+certPath = '/etc/letsencrypt/live/ss.pxl.plus/fullchain.pem';
+domain = 'ss.pxl.plus';
 port = 420;
 
 const options = {
@@ -26,22 +27,27 @@ const options = {
     cert: fs.readFileSync(certPath)
 };
 
+const con = mysql.createConnection({
+    host: 'localhost',
+    user:'ss.pxl.plus.server',
+    password: 'Gy9jG208omZ24H7M',
+    database: 'ss.pxl.plus.server'
+});
+
+const privatePath = 'assets/jwtRS512.key';
+const pubPath = 'assets/jwtRS512.key.pub';
+
 const httpsServer = https.createServer(options, app);
 
-httpsServer.listen(port, ip.address());
-console.log(`Server running at ${ip.address()}:${port}`);
+httpsServer.listen(port, domain);
+console.log(`Server running at ${domain + port}`);
 
 
 
 
 function registerRoutes() {
-    const appURL = "https://pxl.plus";
     router.all('*', (req, res, next) => {
-        if (req.headers.origin != appURL) {
-            res.status(403).end();
-            return;
-        }
-
+        console.log('New Request', req.body);
         next();
     });
 
@@ -49,17 +55,25 @@ function registerRoutes() {
         res.status(403).send();
     });
 
-    router.get('/api', async (req, res) => {
+    router.get('/api', (req, res) => {
         //TODO Authorize
         let paths = {routes: []};
         for (let stack of router.stack) {
-            paths.routes.push(stack.route.path);
+            const route = stack.route;
+
+            if (route.path === '*') continue;
+            if (!paths.routes.includes(route.path))
+                paths.routes.push(route.path);
         }
 
         res.send(paths);
     });
+    router.post('/api', (req, res) => {
+        res.status(400).send({router: "Route method not supported"});
+    });
 
-    const privatePath = './assets/private.key';
+
+
     router.post('/api/login', (req, res) => {
         const user = req.body.username;
         const pass = req.body.password;
@@ -68,28 +82,46 @@ function registerRoutes() {
             res.status(400).send('Unable to find required data!');
             return;
         }
-        const key = fs.readFileSync(privatePath);
-        const token = jwt.sign(
-            {data: 'foobar'},
-            key,
-            {expiresIn: '1h', algorithm: 'RS256'}, (err, token) => {
+
+        con.connect(err => {
+           if (err) throw err;
+           console.log('Connected To The DB');
+        });
+
+        let sql = `SELECT * from users where username=? OR email=? AND password=?;`;
+        con.query(sql, [user, user, pass], (err, result) => {
+            if (err) throw err;
+            console.log(result);
+        });
+
+
+        const key = fs.readFileSync(privatePath, {encoding:'utf8'});
+        jwt.sign({data:{username: user, password: pass}}, key, {expiresIn: '1h', algorithm: 'RS256'},
+            (err, token) => {
                 res.send({token: token});
             });
+
     });
 
     router.get('/api/verify', (req, res) => {
-       const token = req.get('token');
+        const token = req.get('token');
         if (token == null) {
-           res.status(400).send('Error: Missing Token!');
-           return;
-       }
+            res.status(400).send('Error: Missing Token!');
+            return;
+        }
+        const key = fs.readFileSync(pubPath, {encoding:'utf8'});
+
+        jwt.verify(token, key,{algorithms: ['RS256']}, (err, decoded) => {
+            console.log(err, decoded);
+            res.send(decoded);
+        });
 
 
 
 
 
     });
-    
+
 
 }
 
