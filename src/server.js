a36ddc1e24740fdaeada42716e37293ff18f5494
TroyG <troy.gidney@lethbridgecollege.ca>
2020-05-05 01:57:10 -0600

    Added google charts chart with test data for formatting Added mysql pool to the server Added logic to add shop information to the database via the server

--- src/server.js
+++ src/server.js
@@ -9,15 +9,6 @@ const bodyparser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
 
-app.use(cors());
-app.use(bodyparser.json());
-
-
-registerRoutes();
-
-
-app.use('/', router);
-
 keyPath = '/etc/letsencrypt/live/ss.pxl.plus/privkey.pem';
 certPath = '/etc/letsencrypt/live/ss.pxl.plus/fullchain.pem';
 domain = 'ss.pxl.plus';
@@ -29,45 +20,46 @@ const options = {
 };
 
 const availableServers = [
-    'pokedash',
-    'pokeclub',
-    'pokelegends',
-    'pokebrawl',
-
+    'dash',
+    'club',
+    'legends',
+    'brawl',
+    'verse'
 ];
 
-const con = mysql.createConnection({
+const mysqlConfig = {
     host: 'localhost',
     user:'ss.pxl.plus.server',
     password: 'Gy9jG208omZ24H7M',
     database: 'ss.pxl.plus.server'
-});
+};
+
+const mysqlPool = mysql.createPool(mysqlConfig);
 
 const privatePath = 'assets/jwtRS512.key';
 const pubPath = 'assets/jwtRS512.key.pub';
-
 const httpsServer = https.createServer(options, app);
+
+
+app.use(cors());
+app.use(bodyparser.json());
+
+
+registerRoutes();
+
+
+app.use('/', router);
+
+
 
 httpsServer.listen(port, domain);
 console.log(`Server running at ${domain + ':' + port}`);
 
-function connectToDB() {
-    con.connect(err => {
-        if (err) {
-            con.resume();
-        }
-        console.log('Connected To The DB');
-    });
-}
 
-function closeDB() {
-    con.pause();
-    console.log('Paused connection');
-}
 
 function registerRoutes() {
     router.all('*', (req, res, next) => {
-        console.log('New Request', req.headers, req.body);
+        console.log('New Request\n', req.headers, req.body);
         next();
     });
 
@@ -102,23 +94,27 @@ function registerRoutes() {
             res.status(400).send('Unable to find required data!');
             return;
         }
-        connectToDB();
 
-        let sql = `SELECT * from users where username=? OR email=? AND password=?;`;
-        con.query(sql, [user, user, pass], (err, result) => {
+        mysqlPool.getConnection((err, connection) => {
             if (err) throw err;
-            if (result.length === 1) {
-                const key = fs.readFileSync(privatePath, {encoding:'utf8'});
-                jwt.sign({data:{username: result[0].username, id: result[0]['user_id'], valid: true}}, key, {expiresIn: '1d', algorithm: 'RS256'},
-                    (err, token) => {
-                        res.send({token: token});
-                        closeDB();
-                    });
-            } else {
-                closeDB();
-                res.status(401).send('Username or Password is incorrect');
-            }
-        });
+            let sql = `SELECT * from users where username=? OR email=? AND password=?;`;
+            connection.query(sql, [user, user, pass], (err2, result) => {
+                if (err2) throw err2;
+                if (result.length === 1) {
+                    const key = fs.readFileSync(privatePath, {encoding:'utf8'});
+                    jwt.sign({data:{username: result[0].username, id: result[0]['user_id'], valid: true}}, key, {expiresIn: '1d', algorithm: 'RS256'},
+                        (err, token) => {
+                            res.send({token: token});
+                            connection.release();
+
+                            if (err2) throw err2;
+                        });
+                } else {
+                    connection.release();
+                    res.status(401).send('Username or Password is incorrect');
+                }
+            });
+        })
     });
 
     router.get('/api/verify', (req, res) => {
@@ -127,31 +123,86 @@ function registerRoutes() {
             res.status(400).send('Error: Missing Token!');
             return;
         }
-        const key = fs.readFileSync(pubPath, {encoding:'utf8'});
 
-        jwt.verify(token, key,{algorithms: ['RS256']}, (err, decoded) => {
-            console.log(err, decoded);
-            res.send(decoded);
-        });
+        res.send(verifyToken(token));
     });
 
 
-    const logFile = `ecotracker.json`;
     router.post('/api/ecotracker', (req, res) => {
+        // let logFile = `src/data/ecotracker_${getFormattedDate()}.json`;
         // TODO Make it so the servers can only use the route
         const server = req.get('server');
         if (server === null || !availableServers.includes(server)) {
             res.status(403).end();
             return;
         }
-        let body = "%" + JSON.stringify(req.body) + "\n";
+        let arrayBody = [];
+        arrayBody.push(req.body.action);
+        arrayBody.push(req.body.totalPrice);
+        arrayBody.push(req.body.itemName);
+        arrayBody.push(req.body.itemQuantity);
+        arrayBody.push(getFormattedDate());
 
-        fs.appendFile(logFile, body, err => {
-                if (err) { console.log(err); }
+        console.log(arrayBody);
+
+        mysqlPool.getConnection((err, connection) => {
+           if (err) throw err;
+           let sql = "INSERT INTO ecotracker_poke"+ server +" VALUES ('"+ req.body.action +"','"+ req.body.totalPrice +"','"+ req.body.itemName +"','"+ req.body.itemQuantity +"','"+ getFormattedDate() +"');";
+           console.log(sql);
+            connection.query(sql, (err2, result) =>{
+                if (err2) throw err2;
+                res.send();
+
+                console.log(result);
+                connection.release();
+            });
         });
-        res.send('Sent to server!');
+
+
+
+        // console.log('Post EcoTracker Called');
+        // let body = "%" + JSON.stringify(req.body) + "\n";
+        //
+        // fs.appendFile(logFile, body, err => {
+        //     if (err) { console.log(err); }
+        //     res.send('Sent to server!');
+        // });
     });
 
+    router.get('/api/ecotracker', (req, res) => {
 
+       console.log('Get EcoTracker endpoint called');
+       res.send();
+    });
+
+    function verifyToken(token) {
+        const key = fs.readFileSync(pubPath, {encoding:'utf8'});
+
+        jwt.verify(token, key,{algorithms: ['RS256']}, (err, decoded) => {
+            if (err) return err;
+            else return decoded;
+        });
+    }
+
+
+
+    function getFormattedDate() {
+        // current date
+        const date_ob = new Date();
+        // adjust 0 before single digit date
+        const date = ("0" + date_ob.getDate()).slice(-2);
+        // current month
+        const month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+        // current year
+        const year = date_ob.getFullYear();
+        // current hours
+        const hours = date_ob.getHours();
+        // current minutes
+        const minutes = date_ob.getMinutes();
+        // current seconds
+        const seconds = date_ob.getSeconds();
+
+        return `${year}-${month}-${date}`;
+    }
 }
 
